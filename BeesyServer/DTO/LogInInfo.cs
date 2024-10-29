e3460d1a8cbbee0f2e6117786583aff631a41938
User <User@LAB3-3>
2024-10-29 12:37:35 +0200

    h

--- BeesyServer/DTO/LogInInfo.cs
+++ BeesyServer/DTO/LogInInfo.cs
@@ -2,7 +2,7 @@
 {
     public class LoginInfo
     {
-        public string UserEmail { get; set; }
-        public string UserPassword { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
     }
 }
